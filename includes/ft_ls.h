1652b2749559e0cbc4ae4f4e68922833ebc9b856
olga <olga-p-98@mail.ru>
2019-07-11 23:51:12 +0300

    added ft_create_new_file

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: sghezn <sghezn@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 16:51:16 by sghezn            #+#    #+#             */
-/*   Updated: 2019/07/11 23:32:05 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/07/11 23:50:54 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,8 @@ typedef struct	s_file
 int		ft_index(char c, char *str);
 int		ft_raise_error(char *str, int error);
 int		*ft_parse_options(int argc, char **argv);
+char    *ft_get_path(char *name);
+t_file  *ft_create_new_file(char *name, char *path, t_stat stat);
 t_file	*ft_create_file_list(int argc, char **argv);
 
 #endif
--- srcs/file_list.c
+++ srcs/file_list.c
@@ -6,8 +6,41 @@
 /*   By: sghezn <sghezn@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 23:29:57 by sghezn            #+#    #+#             */
-/*   Updated: 2019/07/11 23:33:35 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/07/11 23:50:54 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+char    *ft_get_path(char *name)
+{
+
+}
+
+/*
+** A function that creates a new file, given name, path and stat.
+*/
+
+t_file  *ft_create_new_file(char *name, char *path, t_stat stat)
+{
+    t_file  *file;
+
+    if (!((file = (t_file*)malloc(sizeof(t_file) + 1))
+    || (file->name = ft_strdup(name))))
+        ft_raise_error(NULL, 2);
+    file->mode = stat->st_mode;
+    file->n_links = stat->st_nlink;
+    file->user_id = stat->st_uid;
+    file->group_id = stat->st_gid;
+    file->size = stat->st_size;
+    file->atime = stat->st_atime;
+    file->mtime = stat->st_mtime;
+    file->path = ft_get_path(file);
+    file->next = NULL;
+    return (file)
+}
+
+t_file	*ft_create_file_list(int argc, char **argv)
+{
+    
+}
