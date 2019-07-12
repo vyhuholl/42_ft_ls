38e534a97ab6d53bf372d3d4120a4a7a6de8e072
olga <olga-p-98@mail.ru>
2019-07-12 11:03:40 +0300

    added ft_parse_options

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sghezn <sghezn@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 16:48:00 by sghezn            #+#    #+#             */
-/*   Updated: 2019/07/12 10:49:34 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/07/12 11:03:09 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,22 @@
 t_options	*ft_parse_options(int argc, char **argv)
 {
 	t_options	*options;
+	int			*flags;
 	int			i;
 
-	options = (t_options*)malloc(sizeof(t_options) + 1)
+	options = (t_options*)malloc(sizeof(t_options) + 1);
+	flags = (int*)malloc(sizeof(int) * 8 + 1);
+	options->flags = flags;
+	i = 1;
+	while (i < argc)
+	{
+		if (argv[i][0] == '-')
+			ft_add_options(argv[i], options);
+		else if (ft_is_file(argv[i]) == 1)
+			ft_add_file(argv[i], options);
+		else if (ft_is_dir(argv[i]) == 1)
+			ft_add_dir(argv[i], options);
+	}
 	return (options);
 }
 
