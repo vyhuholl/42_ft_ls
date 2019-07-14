57d5150159189aded0a16b009e62a11f4a2fde29
olga <olga-p-98@mail.ru>
2019-07-14 20:05:08 +0300

    added ft_show_file

--- srcs/show.c
+++ srcs/show.c
@@ -6,13 +6,21 @@
 /*   By: sghezn <sghezn@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 17:59:45 by sghezn            #+#    #+#             */
-/*   Updated: 2019/07/14 19:24:44 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/07/14 20:04:33 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void    ft_show_files(t_files *files, t_flags *flags)
+void    ft_show_file(t_file *file, t_flags *flags)
+{
+    if (flags->long_format == 1)
+        ft_show_long_format(file, flags);
+    else
+        ft_printf("%s\n", file->name);   
+}
+
+void    ft_show_files(t_file *files, t_flags *flags)
 {
     
 }
