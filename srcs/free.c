40092cb7411ae147b1401a808408b7197dde4761
olga <olga-p-98@mail.ru>
2019-07-14 11:22:30 +0300

    added ft_free_list

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: sghezn <sghezn@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 10:35:11 by sghezn            #+#    #+#             */
-/*   Updated: 2019/07/14 10:40:42 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/07/14 11:22:16 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@ void    ft_free_list(t_list *list)
 {
     t_list  *ptr;
 
-    
+    while (list)
+    {
+        ptr = list->next;
+        ft_memdel((void**)&list->content);
+        ft_memdel((void**)&list);
+        list = ptr;
+    }
 }
