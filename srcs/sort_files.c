68df48f8050a11c98688fb08363fa5d5f835886c
olga <olga-p-98@mail.ru>
2019-07-15 04:30:20 +0300

    fixed all errors in sort_files.c

--- srcs/sort_files.c
+++ srcs/sort_files.c
@@ -6,7 +6,7 @@
 /*   By: sghezn <sghezn@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 15:25:17 by sghezn            #+#    #+#             */
-/*   Updated: 2019/07/15 04:27:20 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/07/15 04:29:58 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,9 @@ void    ft_swap_files(t_file *file_1, t_file *file_2)
 {
     t_file  *temp;
 
-    temp->name = file_1->name;
-    temp->path = file_1->path;
-    temp->username = file_1->username;
-    temp->groupname = file_1->groupname;
-    temp->stats = file_1->stats;
-    file_1->name = file_2->name;
-    file_1->path = file_2->path;
-    file_1->username = file_2->username;
-    file_1->groupname = file_2->groupname;
-    file_1->stats = file_2->stats;
-    file_2->name = temp->name;
-    file_2->path = temp->path;
-    file_2->username = temp->username;
-    file_2->groupname = temp->groupname;
-    file_2->stats = temp->stats;
+    temp = file_1;
+    file_1 = file_2;
+    file_2 = temp;
 }
 
 void    ft_sort_files(t_file *files, int (*cmp)(const char*, const char*))
