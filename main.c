7d9cd9754dce67806bf80f168b693c402f9888f8
vyhuholl <olga-p-98@mail.ru>
2019-09-14 12:54:26 +0300

    minor fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sghezn <sghezn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 07:58:00 by sghezn            #+#    #+#             */
-/*   Updated: 2019/09/14 12:48:34 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/09/14 12:53:26 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,8 @@ int		main(int argc, char **argv)
 	file_list = ft_file_list(argc, argv, flags);
 	files = ft_only_files(&file_list);
 	ft_print_files(&files, flags);
-	ft_putchar('\n');
+	if (files)
+		ft_putchar('\n');
 	ft_print_all(file_list, flags, 2, argc);
 	ft_free_files(file_list);
 	return (0);
