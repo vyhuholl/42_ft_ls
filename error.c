53c3b505d1f8ee9c644e7dae106827e375d2d5a1
vyhuholl <olga-p-98@mail.ru>
2019-09-14 16:19:45 +0300

    rewrote error handling

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: sghezn <sghezn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/20 16:02:02 by sghezn            #+#    #+#             */
-/*   Updated: 2019/09/03 17:58:51 by sghezn           ###   ########.fr       */
+/*   Updated: 2019/09/14 16:17:19 by sghezn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,24 @@
 
 void	ft_memory_error(void)
 {
-	t_stat	std_out;
-	t_stat	std_err;
-
 	ft_putstr_fd(strerror(ENOMEM), 2);
-	if (fstat(STDOUT_FILENO, &std_out) == 0 &&
-		fstat(STDERR_FILENO, &std_err) == 0 &&
-		S_ISCHR(std_out.st_mode) &&
-		S_ISCHR(std_err.st_mode) &&
-		(std_out.st_dev != std_err.st_dev ||
-		std_out.st_ino != std_err.st_ino))
+	if (STDOUT_FILENO != STDERR_FILENO)
 		exit(EXIT_FAILURE);
 }
 
 void	ft_options_error(char op)
 {
-	t_stat	std_out;
-	t_stat	std_err;
-
 	ft_putstr_fd("ls: illegal option -- ", 2);
 	ft_putchar_fd(op, 2);
 	ft_putchar_fd('\n', 2);
 	ft_putstr_fd("usage: ls [-ABCFGHLOPRSTUW", 2);
-	ft_putstr_fd("abcdefghiklmnopqrstuwx1] [file ...]", 2);
-	if (fstat(STDOUT_FILENO, &std_out) == 0 &&
-		fstat(STDERR_FILENO, &std_err) == 0 &&
-		S_ISCHR(std_out.st_mode) &&
-		S_ISCHR(std_err.st_mode) &&
-		(std_out.st_dev != std_err.st_dev ||
-		std_out.st_ino != std_err.st_ino))
+	ft_putstr_fd("abcdefghiklmnopqrstuwx1] [file ...]\n", 2);
+	if (STDOUT_FILENO != STDERR_FILENO)
 		exit(EXIT_FAILURE);
 }
 
 void	ft_not_found_error(t_list *files)
 {
-	t_stat	std_out;
-	t_stat	std_err;
-
 	ft_lstreverse(&files);
 	while (files)
 	{
@@ -59,43 +40,22 @@ void	ft_not_found_error(t_list *files)
 		ft_putstr_fd(": No such file or directory\n", 2);
 		files = files->next;
 	}
-	if (fstat(STDOUT_FILENO, &std_out) == 0 &&
-		fstat(STDERR_FILENO, &std_err) == 0 &&
-		S_ISCHR(std_out.st_mode) &&
-		S_ISCHR(std_err.st_mode) &&
-		(std_out.st_dev != std_err.st_dev ||
-		std_out.st_ino != std_err.st_ino))
+	if (STDOUT_FILENO != STDERR_FILENO)
 		exit(EXIT_FAILURE);
 }
 
 void	ft_fts_error(void)
 {
-	t_stat	std_out;
-	t_stat	std_err;
-
-	ft_putstr_fd("ls: fts_open: No such file or directory", 2);
-	if (fstat(STDOUT_FILENO, &std_out) == 0 &&
-		fstat(STDERR_FILENO, &std_err) == 0 &&
-		S_ISCHR(std_out.st_mode) &&
-		S_ISCHR(std_err.st_mode) &&
-		(std_out.st_dev != std_err.st_dev ||
-		std_out.st_ino != std_err.st_ino))
+	ft_putstr_fd("ls: fts_open: No such file or directory\n", 2);
+	if (STDOUT_FILENO != STDERR_FILENO)
 		exit(EXIT_FAILURE);
 }
 
 void	ft_permission_error(char *name)
 {
-	t_stat	std_out;
-	t_stat	std_err;
-
 	ft_putstr_fd("ls: ", 2);
 	ft_putstr_fd(name, 2);
 	ft_putstr_fd(": Permission denied\n", 2);
-	if (fstat(STDOUT_FILENO, &std_out) == 0 &&
-		fstat(STDERR_FILENO, &std_err) == 0 &&
-		S_ISCHR(std_out.st_mode) &&
-		S_ISCHR(std_err.st_mode) &&
-		(std_out.st_dev != std_err.st_dev ||
-		std_out.st_ino != std_err.st_ino))
+	if (STDOUT_FILENO != STDERR_FILENO)
 		exit(EXIT_FAILURE);
 }
